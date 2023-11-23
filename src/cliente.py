3f7691a2f9db9c2a6f93382b1d869ed86e611edf
Elena <elenabrugarolas@gmail.com>
2023-11-23 20:10:42 +0100

    Servidor asincrono

--- src/cliente.py
+++ src/cliente.py
@@ -14,10 +14,11 @@ port = 12345
 
 BUFFER_SIZE = 1024
 
-MESSAGE = "Hola mundo" #Esto es lo que vamos a enviar
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
-	socket_tcp.connect((host,port))
-	socket_tcp.send(MESSAGE.encode("utf-8"))
-	data = socket_tcp.recv(BUFFER_SIZE)
-	print(data.decode("utf-8"))
+	while True:
+		socket_tcp.connect((host,port))
+		while True:
+			MESSAGE = input("Escriba los datos para mandar:") #Esto es lo que vamos a enviar
+			socket_tcp.send(MESSAGE.encode("utf-8"))
+			data = socket_tcp.recv(BUFFER_SIZE)
+			print(data.decode("utf-8"))
