a2b52a0bd3732a953b29582e3768c5201afd059d
Elena <elenabrugarolas@gmail.com>
2023-11-23 19:37:27 +0100

    añado cliente y servidor

--- examples/semana-1.py
+++ examples/semana-1.py
@@ -1,2 +1,24 @@
 
-print("Hola mundo!!!")
+
+compi = "Pepito"
+
+print("Hola mundo!!!" + compi)
+
+lista = [1,2,3,4]
+lista.append(5)  #añade elementos a una lista, las tuplas no son modificables 
+
+# funcion 
+def saluda(compi):
+    print (f"Hola {compi}")
+
+saluda ('pepe')
+
+dict = { #diccionario 
+    'a' : 'es a ',
+    'b' : 'es b'
+}
+try:
+    print (dict.get('c'))
+
+except: 
+    print('Aqui ha pasado algo')
---
+++ src/auxiliar.py
@@ -0,0 +1,11 @@
+
+
+
+def suma(a,b):
+    return a + b
+
+
+if (__name__ == '__main__'):
+
+# prueba
+    print(suma(1,2))
